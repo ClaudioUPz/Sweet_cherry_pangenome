4a92e1cb35fbc4193a5be7afc5e8e10345a8c331
ClaudioUPz <claud.urra@gmail.com>
2026-03-19 15:46:04 -0300

    Update extract_SNP.sh

--- scripts/06_snp_analysis/extract_SNP.sh
+++ scripts/06_snp_analysis/extract_SNP.sh
@@ -8,32 +8,29 @@
 
 set -euo pipefail
 
-# Cargar módulo
+# Load module
 module load bcftools
 
-BASE="/home/curra/pangenome"
-IN_DIR="$BASE/5.pangenome"
-OUT_DIR="$BASE/6.SNP"
+BASE="/path/to/basedir"
+IN_DIR="$BASE/pangenome_dir"
+OUT_DIR="$BASE/SNP_dir"
 
 mkdir -p "$OUT_DIR" logs
 
 for chr in {1..8}; do
   in_vcf="$IN_DIR/chr${chr}/output/chr${chr}_run.vcf.gz"
   if [[ ! -f "$in_vcf" ]]; then
-    echo "ERROR: No existe $in_vcf"; exit 1
+    echo "ERROR: Doesn't exist $in_vcf"; exit 1
   fi
 
   out_chr_dir="$OUT_DIR/chr${chr}"
   mkdir -p "$out_chr_dir"
 
-  # SNPs (mantiene bi y multi-alélicos). Indexa con tabix.
+  # SNPs (keep bi y multi-allelic). Index with tabix.
   bcftools view -v snps -Oz -o "$out_chr_dir/snps_chr${chr}.vcf.gz" "$in_vcf"
   bcftools index -t "$out_chr_dir/snps_chr${chr}.vcf.gz"
 
-  # Si SOLO dejamos bialélicos, descomentar la línea siguiente en lugar de la anterior:
-  # bcftools view -v snps -m2 -M2 -Oz -o "$out_chr_dir/snps_chr${chr}.vcf.gz" "$in_vcf" && bcftools index -t "$out_chr_dir/snps_chr${chr}.vcf.gz"
-
-  echo "Listo chr${chr}: $out_chr_dir/snps_chr${chr}.vcf.gz"
+  echo "Done chr${chr}: $out_chr_dir/snps_chr${chr}.vcf.gz"
 done
 
-echo "✓ VCFs con solo SNPs en pangenome/6.SNP/chr*/"
+echo "✓ VCFs with only SNP in pangenome_dir/SNP_dir/chr*/"
